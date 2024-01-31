e802185bfdff964ea334984b578638f7cc9fec40
VimalRaghunath <vimalvrk@gmail.com>
2024-01-31 10:16:41 +0530

    daily updation in frontend

--- src/Adminusers.js
+++ src/Adminusers.js
@@ -1,44 +1,74 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
+import { useCookies } from 'react-cookie'
+import { AxiosInstance } from './Components/AxiosInstance'
 
-// function Adminusers() {
+function Adminusers() {
 
-//     const [userslist,setUserslist] = useState()
+    const [usersList,setUsersList] = useState()
+    const [cookie,setCookie] = useCookies(["cookies"])
 
-//   return (
-// //     <div>
-// // {/*       
-// //   //   <div>
-// //   //   {/* <div>
-// //   //     <Sidebar/>
-// //   //     </div> */}
+    const allUsers = async () => {
+        try {
+            const alltheUsers = await AxiosInstance.get("/api/admin/users", {
 
-// //   //     <TableContainer component={Paper}>
-// //   //     <Table sx={{ minWidth: 700 }} aria-label="customized table">
-// //   //       <TableHead>
-// //   //         <TableRow>
-// //   //           <StyledTableCell>Name</StyledTableCell>
-// //   //           <StyledTableCell align="right">Username</StyledTableCell>
-// //   //           <StyledTableCell align="right">Email</StyledTableCell>
-// //   //           <StyledTableCell align="right">mobile</StyledTableCell>
-// //   //         </TableRow>
-// //   //       </TableHead>
-// //   //       <TableBody>
-// //   //         {followerlist.map((row) => (
-// //   //           <StyledTableRow key={row._id}>
-// //   //             <StyledTableCell component="th" scope="row">
-// //   //               {row.name}
-// //   //             </StyledTableCell>
-// //   //             <StyledTableCell align="right">{row.username}</StyledTableCell>
-// //   //             <StyledTableCell align="right">{row.email}</StyledTableCell>
-// //   //             <StyledTableCell align="right">{row.mobile}</StyledTableCell>
-// //   //           </StyledTableRow>
-// //   //         ))}
-// //   //       </TableBody>
-// //   //     </Table>
-// //   //   </TableContainer>
-// //   // </div> */}
-// //     </div>
-//   )
-// }
+                headers: {
+                   Authorization: `bearer ${cookie.cookies}`,
+                },
+             }
+          );
+           return setUsersList()
+        } catch (error) {
+            
+        }
+    }
 
-// export default Adminusers
+
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Adminusers
+
+
+
+
+//     <div>
+      
+//     <div>
+//      <div> 
+//       <Sidebar/>
+//       </div> 
+
+//   //     <TableContainer component={Paper}>
+//   //     <Table sx={{ minWidth: 700 }} aria-label="customized table">
+//   //       <TableHead>
+//   //         <TableRow>
+//   //           <StyledTableCell>Name</StyledTableCell>
+//   //           <StyledTableCell align="right">Username</StyledTableCell>
+//   //           <StyledTableCell align="right">Email</StyledTableCell>
+//   //           <StyledTableCell align="right">mobile</StyledTableCell>
+//   //         </TableRow>
+//   //       </TableHead>
+//   //       <TableBody>
+//   //         {followerlist.map((row) => (
+//             <StyledTableRow key={row._id}>
+//               <StyledTableCell component="th" scope="row">
+//                 {row.name}
+//               </StyledTableCell>
+//               <StyledTableCell align="right">{row.username}</StyledTableCell>
+//               <StyledTableCell align="right">{row.email}</StyledTableCell>
+//               <StyledTableCell align="right">{row.mobile}</StyledTableCell>
+//             </StyledTableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </TableContainer>
+//   </div>
+//     </div>
+ 
+
