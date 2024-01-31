1d33b1ffc3fb816886b454084b5ced90a97c028d
VimalRaghunath <vimalvrk@gmail.com>
2024-01-31 12:01:59 +0530

    admin users list added

--- src/Adminusers.js
+++ src/Adminusers.js
@@ -1,11 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useCookies } from 'react-cookie'
 import { AxiosInstance } from './Components/AxiosInstance'
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Avatar } from '@mui/material';
+
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
 
 function Adminusers() {
 
-    const [usersList,setUsersList] = useState()
+    const [usersList,setUsersList] = useState([])
     const [cookie,setCookie] = useCookies(["cookies"])
+
 
     const allUsers = async () => {
         try {
@@ -16,7 +49,7 @@ function Adminusers() {
                 },
              }
           );
-           return setUsersList(alltheUsers.data._id)
+           return setUsersList(alltheUsers.data)
         } catch (error) {
 
           console.error("Error fetching all the users:", error);
@@ -24,11 +57,41 @@ function Adminusers() {
         }
     }
 
+    useEffect(() => {
+      allUsers();
+    }, []); 
+
 
   return (
-    <div>
+    <div style={{display:"flex",justifyContent:"center"}}>
       <div>
-        
+      <TableContainer component={Paper} >
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell align="right">Username</StyledTableCell>
+            <StyledTableCell align="right">Email</StyledTableCell>
+            <StyledTableCell align="right">Mobile</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+          usersList.map((item) => (
+            <StyledTableRow key={item._id}>
+              <StyledTableCell component="th" scope="row">
+                <Avatar src={item.Avatar}/>
+                {item.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{item.username}</StyledTableCell>
+              <StyledTableCell align="right">{item.email}</StyledTableCell>
+              <StyledTableCell align="right">{item.mobile}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+       
       </div>
     </div>
   )
@@ -39,38 +102,6 @@ export default Adminusers
 
 
 
-//     <div>
-      
-//     <div>
-//      <div> 
-//       <Sidebar/>
-//       </div> 
 
-//   //     <TableContainer component={Paper}>
-//   //     <Table sx={{ minWidth: 700 }} aria-label="customized table">
-//   //       <TableHead>
-//   //         <TableRow>
-//   //           <StyledTableCell>Name</StyledTableCell>
-//   //           <StyledTableCell align="right">Username</StyledTableCell>
-//   //           <StyledTableCell align="right">Email</StyledTableCell>
-//   //           <StyledTableCell align="right">mobile</StyledTableCell>
-//   //         </TableRow>
-//   //       </TableHead>
-//   //       <TableBody>
-//   //         {followerlist.map((row) => (
-//             <StyledTableRow key={row._id}>
-//               <StyledTableCell component="th" scope="row">
-//                 {row.name}
-//               </StyledTableCell>
-//               <StyledTableCell align="right">{row.username}</StyledTableCell>
-//               <StyledTableCell align="right">{row.email}</StyledTableCell>
-//               <StyledTableCell align="right">{row.mobile}</StyledTableCell>
-//             </StyledTableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   </div>
-//     </div>
  
 
--- src/Components/Admindashboard.js
+++ src/Components/Admindashboard.js
@@ -14,6 +14,7 @@ export default function Admindashboard() {
       width={600}
       height={350}
     />
+    
 
   );
 }
